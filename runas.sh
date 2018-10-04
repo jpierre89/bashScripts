40b2acd225b82bff7816a79f6715dd2b879891c0
jon <jonpierre@gmail.com>
2018-10-04 13:51:40 -0500

    comment example

--- runas.sh
+++ runas.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Compile, Link, Cean, Run, Echo.
-# Must use source name without file extension.
 # ARMv8 processor Assembler and linker.
+# Example: $ runas.sh fileName // remove .s extension
 
 aarch64-linux-gnu-as -o	$1.o $1.s
 aarch64-linux-gnu-gcc -static -o $1 $1.o
